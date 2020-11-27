1c4ec430cee3b5bc243e38c82c31a48c225cc4a3
s-grig <1659192@mail.ru>
2020-11-27 10:49:21 +0300

    gitignore

--- out/build/x64-Debug/cmake_install.cmake
+++ out/build/x64-Debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: C:/DATA/GitHub_Stas/projekt
+# Install script for directory: E:/GitHub/projekt
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "C:/DATA/GitHub_Stas/projekt/out/install/x64-Debug")
+  set(CMAKE_INSTALL_PREFIX "E:/GitHub/projekt/out/install/x64-Debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -40,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/DATA/GitHub_Stas/projekt/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/GitHub/projekt/out/build/x64-Debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
